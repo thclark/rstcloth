26287537725ef8e29ce03b05c247a3416a47c7a5
tycho garen <garen@tychoish.com>
2013-05-05 13:44:43 -0400

    adding support for headings and code blocks

--- rstcloth/rstcloth.py
+++ rstcloth/rstcloth.py
@@ -42,14 +42,22 @@ class RstCloth(Cloth):
         self.docs = AttributeDict( { } )
         self.docs._all = [ ]
 
-    def _add(self, string, block='_all'):
-        self.docs._all.append(string)
+    def _add(self, content, block='_all'):
 
-        if block != '_all':
-            if block not in self.docs:
-                self.docs[block] = []
+        def _add_line(line):
+            self.docs._all.append(string)
 
-            self.docs[block] = string
+            if block != '_all':
+                if block not in self.docs:
+                    self.docs[block] = []
+
+                self.docs[block] = content
+
+        if isinstance(content, list):
+            for string in list:
+                _add_line(string)
+        else:
+            _add_line()
 
     def newline(self, count=1, block='_all'):
         if isinstance(count, int):
@@ -111,6 +119,13 @@ class RstCloth(Cloth):
     def _paragraph(content):
         return [ i for i.strip() in fill(content).split('\n') ]
 
+    def codeblock(self, content, indent, language=None):
+        if langauge is None:
+            o = [ '::', _indent(content, 3) ]
+            self._add(_indent(o, indent), block=block)
+        else:
+            self.directive(name='code-block', arg=language, content=content, indent=indent, block=block)
+
     def footnote(self, ref, text, indent, block='_all'):
         self._add(fill('.. [#{0}] {1}'.format(ref, text), indent, indent + 3), block=block)
         self._add(fill('.. [#{0}] {1}'.format(ref, text), indent, indent + 3), block='_footnotes')
@@ -122,7 +137,7 @@ class RstCloth(Cloth):
             name = self.bold(name)
 
         o.append(name)
-        o.append(_indent(text, 3, 3))
+        o.append(_indent(text, 3))
 
         self._add(_indent(o, indent), block)
 
@@ -143,7 +158,7 @@ class RstCloth(Cloth):
         for line in output:
             self._add(_indent(line, indent))
 
-    def content(content, indent, block='_all'):
+    def content(self, content, indent, block='_all'):
         if isinstance(content, list):
             for line in content:
                 self._add(_indent(line, indent), block)
@@ -152,3 +167,28 @@ class RstCloth(Cloth):
 
             for line in lines:
                 self._add(_indent(line, indent), block)
+
+    def title(self, text, char='=', block='_all'):
+        line = char * len(text)
+        self._add([line, text, line], block)
+
+    def heading(self, text, char, block='_all'):
+        self.add([text, char * len(text)])
+
+    def h1(self, text, block='_all'):
+        self.heading(text, char='=', block)
+
+    def h2(self, text, block='_all'):
+        self.heading(text, char='-', block)
+
+    def h3(self, text, block='_all'):
+        self.heading(text, char='~', block)
+
+    def h4(self, text, block='_all'):
+        self.heading(text, char='+')
+
+    def h5(self, text, block='_all'):
+        self.heading(text, char='^')
+
+    def h6(self, text, block='_all'):
+        self.heading(text, char=';')
